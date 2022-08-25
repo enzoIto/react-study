31668942d4b474483c46a48a79799a0a3c5278ac
Enzo Ito <enzo.ito@madeiramadeira.com.br>
2022-08-24 22:13:13 -0300

    feat: Creating Search and List component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,8 +8,46 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
+      <hr />
+
+      <List />
     </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+const list = [... ];
+
+function App() { ... }
+
+function List() {
+  return (
+    <ul>
+      {list.map(function(item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
