5cffd375211e33cd1627f1be2239ddc4da293c13
enzo.ito <enzo.ito@madeiramadeira.com.br>
2022-09-05 17:11:53 -0300

    rendering JS objects inside JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,24 @@
-import logo from './logo.svg';
 import * as React from 'react';
 
-const title = 'React'
-
-import './App.css';
+const welcome = {
+  title: 'React',
+  greeting: 'Hey'
+}
 
 function App() {
   return (
     <div>
-      <h1>My Hacker Stories</h1>
+      <h1>
+        Hello {welcome.title} {welcome.greeting}
+      </h1>
 
-      <Search />
-
-      <hr />
-
-      <List />
-    </div>
-  );
-}
-
-function Search() {
-  return (
-    <div>
-      <label htmlFor='search'>Search: </label>
+      # leanpub-start-insert
+      <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      # leanpub-end-insert
     </div>
   );
 }
-
-const list = [ ... ];
-
-function List() {
-  return (
-    <ul>
-      {list.map(function(item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-
 
 export default App;
+
