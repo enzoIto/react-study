b640a3fae21a63d441bb8ee1d2909ac7c2c2f588
enzo.ito <enzo.ito@madeiramadeira.com.br>
2022-09-21 22:18:23 -0300

    feat: creating Search and List component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,70 @@ import * as React from 'react';
 const welcome = {
   title: 'React',
   greeting: 'Hey'
-}
+};
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello {welcome.title} {welcome.greeting}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
-      # leanpub-start-insert
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      # leanpub-end-insert
+# leanpub-start-insert
+      <Search />
+# leanpub-end-insert
+
+      <hr />
+
+      <List />
     </div>
   );
-}
+};
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id='search' type='text' />
+    </div>
+  );
+};
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default App;
 
