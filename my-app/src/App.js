83567468f72c30fda1050f6dc0c1498b0ac8ac6f
enzo.ito <enzo.ito@madeiramadeira.com.br>
2022-09-24 18:59:23 -0300

    refactoring List Component and passing props to Item component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,39 @@
 import * as React from 'react';
 
-const welcome = {
-  title: 'React',
-  greeting: 'Hey'
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+        <div>
+          <h1>My Hacker Stories</h1>
+
+    # leanpub-start-insert
+          <Search />
+    # leanpub-end-insert
+
+          <hr />
+
+          <List list={stories} />
+        </div>
+      );
 };
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const App = () => (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-# leanpub-start-insert
-      <Search />
-# leanpub-end-insert
-
-      <hr />
-
-      <List />
-    </div>
-  );
 
 const Search = () => {
 
@@ -56,20 +53,24 @@ const Search = () => {
   );
 };
 
-const List = () => (
+const List = (props) => (
     <ul>
-      {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item{...item} />
       ))}
     </ul>
-  );
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 export default App;
 
