99f6f9ba68a968b3193afd56e327020f84bc4568
enzo.ito <enzo.ito@madeiramadeira.com.br>
2022-10-06 00:07:57 -0300

    refactor: refactoring app.js to use destructuring and rest operations

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -56,20 +56,20 @@ const Search = ({ search, onSearch }) => (
 
 const List = ({ list }) => (
   <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map(({ objectID, ...item }) => (
+      <Item key={objectID} {...item} />
     ))}
   </ul>
 );
 
-const Item = ({ item }) => (
+const Item = ({ title, url, author, num_comments, points }) => (
   <li>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 );
 
