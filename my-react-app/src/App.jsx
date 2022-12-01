82ce70d4c74c763f03afad0227723c377aed0f29
Massaki <massakivx@gmail.com>
2022-11-30 21:48:37 -0300

    fetching real data into the application

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,28 +1,7 @@
 import * as React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const getAsyncStories = () =>
-  new Promise((resolve, reject) =>
-    setTimeout(reject,2000)
-  );
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -82,12 +61,13 @@ const App = () => {
   React.useEffect(() => {
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    
-    getAsyncStories()
+
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() =>
