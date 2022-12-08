43ca456b64c61df5b9e3b7889141034d7d099898
Massaki <massakivx@gmail.com>
2022-12-07 21:11:44 -0300

    making the search server-side instead of client-side

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -60,9 +60,11 @@ const App = () => {
 
   React.useEffect(() => {
 
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -72,7 +74,7 @@ const App = () => {
       })
       .catch(() =>
         dispatchStories({type: 'STORIES_FETCH_FAILURE'}));
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -85,13 +87,9 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.data.filter((story) => 
-  story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <div>
-      <h1>Enzo's stories</h1>
+      <h1>Nome aleat</h1>
 
       <InputWithLabel
         id="search"
@@ -109,10 +107,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List
-          list={searchedStories}
-          onRemoveItem={handleRemoveStory}
-        />
+        <List list={stories.data} onRemoveItem={handleRemoveStory}/>
       )}
     </div>
   );
